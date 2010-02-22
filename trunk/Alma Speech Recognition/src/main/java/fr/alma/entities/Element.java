7a9c1eae045316d7cc809ef166ed8f886c2f1982
braud.jeremy <braud.jeremy@a98f104e-f80d-11de-a7af-57dd9f136cd4>
2010-02-22 17:49:04 +0000

    menu contextuel des cours opérationel.

--- trunk/Alma Speech Recognition/src/main/java/fr/alma/entities/Element.java
+++ trunk/Alma Speech Recognition/src/main/java/fr/alma/entities/Element.java
@@ -4,12 +4,15 @@ import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 
 /**
  * Classe Element.
  */
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Element extends AbstractEntity {
 
 	/** Id. */
