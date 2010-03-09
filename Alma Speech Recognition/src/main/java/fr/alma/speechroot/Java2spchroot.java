726c6cc350a3f9d356d0aeb9b0747ad8e09774d4
cedric.krommenhoek <cedric.krommenhoek@a98f104e-f80d-11de-a7af-57dd9f136cd4>
2010-03-09 10:36:51 +0000

    Correction de l'habillage

--- Alma Speech Recognition/src/main/java/fr/alma/speechroot/Java2spchroot.java
+++ Alma Speech Recognition/src/main/java/fr/alma/speechroot/Java2spchroot.java
@@ -2,30 +2,68 @@ package fr.alma.speechroot;
 
 import java.io.File;
 import java.io.IOException;
-
-import fr.alma.speechroot.RegQuery.StreamReader;
+import java.io.InputStream;
+import java.io.StringWriter;
 
 /**
- * Implémentation de l'interface du moteur Speechroot.
+ * Classe de correspondance entre la DLL et Java.
  * @author Cédric Krommenhoek
  */
 public class Java2spchroot implements TreatMessageInterface {
 
+	/**
+	 * Fermeture du microphone
+	 * @return 0 en cas de succès
+	 */
 	public static native int micoff();
+	
+	/**
+	 * Ouverture du microphone.
+	 * @param save_audio_flag Paramètre de sauvegarde du flux audio
+	 * @return 0 en cas de succès
+	 */
 	public static native int micon(String save_audio_flag);
+	
+	/**
+	 * Démarrage du moteur.
+	 * @return 0 en cas de succès
+	 */
 	public static native int startEngine();
+	
+	/**
+	 * Arrêt du moteur.
+	 * @return 0 en cas de succès
+	 */
 	public static native int stopEngine();
+	
 	public static native int RegisterRecognitionCB();
+	
+	/**
+	 * Fonction d'initialisation de l'application.
+	 * @param tmi La classe contenant la fonction de callback
+	 * @param msg Message (à ne pas laisser vide)
+	 * @param text Corps (à ne pas laisser vide)
+	 */
 	public static native void initInterfaceToSmapi(TreatMessageInterface tmi, String msg, String text);
+	
+	/**
+	 * Fermeture de l'application.
+	 */
 	public static native void endInterfaceToSmapi();
+	
 	public static native int AdaptVoiceDialog();
 	public static native int AdaptVocabDialog();
-	
+//	public static native int ManageUserDico();
+
+	/** Initialisation de la requête sur la base de registre. */
 	private static final String REGQUERY_UTIL = "reg query ";
+	/** Clé registre. */
 	private static final String REGSTR_TOKEN = "REG_SZ";
+	/** Emplacement de la clé registre de Speechroot. */
 	private static final String SPEECHROOT_FOLDER = REGQUERY_UTIL +
-	  "\"HKLM\\SOFTWARE\\IBM\\VoiceType\\Engine\\Directories\" /v bin";
-	
+	"\"HKLM\\SOFTWARE\\IBM\\VoiceType\\Engine\\Directories\" /v bin";
+
+	/** Chargement de la DLL. */
 	static {
 		File dll = new File(getSpchrootPath() + "/java2spchroot.dll");
 		try {
@@ -34,27 +72,58 @@ public class Java2spchroot implements TreatMessageInterface {
 			e.printStackTrace();
 		}
 	}
-	
+
+	/**
+	 * Méthode d'accès à l'emplacement de Speechroot à l'aide de la base de registre.
+	 * @return L'emplacement du dossier "bin" de Speechroot
+	 */
 	public static String getSpchrootPath() {
 		Process process;
 		try {
 			process = Runtime.getRuntime().exec(SPEECHROOT_FOLDER);
 			StreamReader reader = new StreamReader(process.getInputStream());			
 			reader.start();
-		    process.waitFor();
-		    reader.join();		    
-		    String result = reader.getResult();
-		    int p = result.indexOf(REGSTR_TOKEN);		    
-		    if (p == -1) {
-		    	return null;
-		    }		    
-		    return result.substring(p + REGSTR_TOKEN.length()).trim();		    
+			process.waitFor();
+			reader.join();		    
+			String result = reader.getResult();
+			int p = result.indexOf(REGSTR_TOKEN);		    
+			if (p == -1) {
+				return null;
+			}		    
+			return result.substring(p + REGSTR_TOKEN.length()).trim();		    
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
+
+	/**
+	 * Classe interne utilisée pour la lecture de la base de registre.
+	 * @author Cédric Krommenhoek
+	 */
+	static class StreamReader extends Thread {
+		private InputStream is;
+		private StringWriter sw;
+
+		StreamReader(InputStream is) {
+			this.is = is;
+			sw = new StringWriter();
+		}
+
+		public void run() {
+			try {
+				int c;
+				while ((c = is.read()) != -1)
+					sw.write(c);
+			}
+			catch (IOException e) { ; }
+		}
+
+		String getResult() {
+			return sw.toString();
+		}
+	}
+
 	/**
 	 * @see fr.alma.speechroot.TreatMessageInterface#treatMsgFunction(java.lang.String, java.lang.String)
 	 */
@@ -64,5 +133,5 @@ public class Java2spchroot implements TreatMessageInterface {
 		System.out.println("Corps : " + body);
 		return 0;
 	}
-	
+
 }
