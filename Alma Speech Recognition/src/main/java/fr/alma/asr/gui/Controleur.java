d45f344b67c4bff53373916fafff6bd8812c21e9
gaetan.hervouet <gaetan.hervouet@a98f104e-f80d-11de-a7af-57dd9f136cd4>
2010-03-16 20:47:40 +0000

    Checkstyle rules

--- Alma Speech Recognition/src/main/java/fr/alma/asr/gui/Controleur.java
+++ Alma Speech Recognition/src/main/java/fr/alma/asr/gui/Controleur.java
@@ -1,5 +1,13 @@
 package fr.alma.asr.gui;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.tree.DefaultMutableTreeNode;
+
 import fr.alma.asr.dao.ElementDao;
 import fr.alma.asr.dao.FolderDao;
 import fr.alma.asr.dao.LessonDao;
@@ -13,17 +21,9 @@ import fr.alma.asr.entities.Lesson;
 import fr.alma.asr.gui.tree.DialogProprietes;
 import fr.alma.asr.utils.FileExporter;
 
-import java.awt.Component;
-import java.io.File;
-import java.util.HashMap;
-import java.util.List;
-
-import javax.swing.JTextArea;
-import javax.swing.JTextPane;
-import javax.swing.tree.DefaultMutableTreeNode;
-
 /**
  * Controleur de l'IHM.
+ * 
  * @author Braud Jeremy
  */
 public final class Controleur {
@@ -31,25 +31,21 @@ public final class Controleur {
 	/** L'instance du controleur. */
 	private static Controleur instance;
 
-	private static HashMap<String, WorkPanel> workPanelList;
-	
-	
 	/*------------------------------*/
 	private String workPlanPosition = "right";
 	private boolean workShowPlan = true;
-	private boolean workShowCourses =true;
-	
-	
+	private boolean workShowCourses = true;
+
 	/**
 	 * Constructeur privé.
 	 */
 	private Controleur() {
 		connexion();
 	}
-	
+
 	/** L'instance du splash screen. */
 	private static SplashScreen splash;
-	
+
 	/**
 	 * Affiche le splash screen.
 	 */
@@ -68,12 +64,13 @@ public final class Controleur {
 
 	/**
 	 * Accès à l'instance du controleur.
+	 * 
 	 * @return l'instance du controleur
 	 */
 	public static Controleur getInstance() {
-		if(instance==null) {
+		if (instance == null) {
 			instance = new Controleur();
-			workPanelList = new HashMap<String, WorkPanel>();
+			new HashMap<String, WorkPanel>();
 		}
 		return instance;
 	}
@@ -83,7 +80,8 @@ public final class Controleur {
 	 */
 	private void connexion() {
 		String chemin = "./DataBase/db";
-		AbstractDaoImpl.addSpecificProperty("hibernate.connection.url", "jdbc:h2:" + chemin);
+		AbstractDaoImpl.addSpecificProperty("hibernate.connection.url",
+				"jdbc:h2:" + chemin);
 		File fichier = new File(chemin + ".h2.db");
 		if (!fichier.exists()) {
 			Folder racine = new Folder("Cours");
@@ -98,16 +96,18 @@ public final class Controleur {
 		AbstractDaoImpl.deconnexion();
 	}
 
-	/* ---------------------------------------------------------*/
-	/* ---------------Gestion du panel d'info-------------------*/
-	/* ---------------------------------------------------------*/
+	/* --------------------------------------------------------- */
+	/* ---------------Gestion du panel d'info------------------- */
+	/* --------------------------------------------------------- */
 
 	/** Le panel d'info. */
 	private static StatusPanel panelInfo;
 
 	/**
 	 * Enregistrement du panel d'info.
-	 * @param info le PanelInfo
+	 * 
+	 * @param info
+	 *            le PanelInfo
 	 */
 	public static void setStatusPanel(StatusPanel info) {
 		panelInfo = info;
@@ -119,13 +119,15 @@ public final class Controleur {
 		}
 	}
 
-	/* ---------------------------------------------------------*/
-	/* -----------------Gestion de l'arbre----------------------*/
-	/* ---------------------------------------------------------*/
-	
+	/* --------------------------------------------------------- */
+	/* -----------------Gestion de l'arbre---------------------- */
+	/* --------------------------------------------------------- */
+
 	/**
 	 * Construit l'arbre des cours.
-	 * @param racine la racine de l'arbre.
+	 * 
+	 * @param racine
+	 *            la racine de l'arbre.
 	 */
 	public void construireArbreCours(DefaultMutableTreeNode racine) {
 		FolderDao dao = new FolderDaoImpl();
@@ -136,12 +138,16 @@ public final class Controleur {
 
 	/**
 	 * Fonction auxiliaire pour construire l'arbre des cours.
-	 * @param racine la racine courante.
-	 * @param Folder le Folder courant.
+	 * 
+	 * @param racine
+	 *            la racine courante.
+	 * @param Folder
+	 *            le Folder courant.
 	 */
-	private void construireArbreCoursBis(DefaultMutableTreeNode racine, Folder Folder) {
+	private void construireArbreCoursBis(DefaultMutableTreeNode racine,
+			Folder folder) {
 		ElementDao dao = new ElementDaoImpl();
-		for (Element elem : dao.findAllOfDossier(Folder)) {
+		for (Element elem : dao.findAllOfDossier(folder)) {
 			DefaultMutableTreeNode rep;
 			if (elem.isFile()) {
 				rep = new DefaultMutableTreeNode(elem, false);
@@ -155,40 +161,49 @@ public final class Controleur {
 
 	/**
 	 * Détruit un élément de l'arbre.
-	 * @param node le noeud sélectionné
+	 * 
+	 * @param node
+	 *            le noeud sélectionné
 	 */
 	public void suppressionElement(DefaultMutableTreeNode node) {
 		ElementDao dao = new ElementDaoImpl();
 		Element element = (Element) node.getUserObject();
-		Folder Folder = element.getDossierConteneur();
-		Folder.removeElement(element);
-		dao.update(Folder);
+		Folder folder = element.getDossierConteneur();
+		folder.removeElement(element);
+		dao.update(folder);
 		dao.delete(element.getId());
 	}
 
 	/**
 	 * Fonction d'ajout de fichier.
-	 * @param nom le nom du fichier à créer
-	 * @param node le noeud parent
+	 * 
+	 * @param nom
+	 *            le nom du fichier à créer
+	 * @param node
+	 *            le noeud parent
 	 * @return l'objet créé
 	 */
 	public Object ajoutFichier(String nom, DefaultMutableTreeNode node) {
 		Lesson file = new Lesson(nom);
-		Folder Folder = (Folder) node.getUserObject();
-		file.setDossierConteneur(Folder);
-		Folder.addElements(file);
+		Folder folder = (Folder) node.getUserObject();
+		file.setDossierConteneur(folder);
+		folder.addElements(file);
 		new LessonDaoImpl().create(file);
-		new FolderDaoImpl().update(Folder);
+		new FolderDaoImpl().update(folder);
 		return file;
 	}
 
 	/**
 	 * Fonction d'ajout de Folder.
-	 * @param nom le nom du Folder à créer
-	 * @param node le noeud parent
+	 * 
+	 * @param nom
+	 *            le nom du Folder à créer
+	 * @param node
+	 *            le noeud parent
 	 * @return l'objet créé
 	 */
-	public Object ajoutFolder(String nom, DefaultMutableTreeNode node, boolean isModule) {
+	public Object ajoutFolder(String nom, DefaultMutableTreeNode node,
+			boolean isModule) {
 		Folder folder = new Folder(nom);
 		Folder conteneur = (Folder) node.getUserObject();
 		folder.setDossierConteneur(conteneur);
@@ -204,211 +219,261 @@ public final class Controleur {
 
 	/**
 	 * Déplace un fichier dans les Folders.
-	 * @param node le noeud à déplacer
-	 * @param parent la cible du noeud
-	 * @param index l'index à laquel on veut insérer le noeud
+	 * 
+	 * @param node
+	 *            le noeud à déplacer
+	 * @param parent
+	 *            la cible du noeud
+	 * @param index
+	 *            l'index à laquel on veut insérer le noeud
 	 */
-	public void deplacerElement(DefaultMutableTreeNode node, DefaultMutableTreeNode cible, int index) {
+	public void deplacerElement(DefaultMutableTreeNode node,
+			DefaultMutableTreeNode cible, int index) {
 		Element element = (Element) node.getUserObject();
-		Folder FolderSource = element.getDossierConteneur();
-		Folder FolderCible = (Folder) cible.getUserObject();
+		Folder folderSource = element.getDossierConteneur();
+		Folder folderCible = (Folder) cible.getUserObject();
 
 		// vérifie si on est plus hors index lors d'une réorganisation par ex
-		if ((index == FolderCible.getElements().size()) && (FolderSource.getId() == FolderCible.getId())) {
+		if ((index == folderCible.getElements().size())
+				&& (folderSource.getId() == folderCible.getId())) {
 			index = index - 1;
 		}
 
 		System.out.println(index);
-		FolderSource.removeElement(element);
-		FolderCible.addElementIndex(element, index);
-		element.setDossierConteneur(FolderCible);
+		folderSource.removeElement(element);
+		folderCible.addElementIndex(element, index);
+		element.setDossierConteneur(folderCible);
 
 		ElementDao dao = new ElementDaoImpl();
 		dao.update(element);
-		dao.update(FolderSource);
-		dao.update(FolderCible);
+		dao.update(folderSource);
+		dao.update(folderCible);
 	}
 
 	/**
 	 * Fonction d'impression.
-	 * @param node le noeud à imprimer
+	 * 
+	 * @param node
+	 *            le noeud à imprimer
 	 */
 	public void impression(DefaultMutableTreeNode node) {
-		//TODO activer l'impression
+		// TODO activer l'impression
 		System.out.println("Impression...");
 	}
 
 	/**
 	 * Affichage des propriétés d'un noeud.
-	 * @param node le noeud sélectionné
+	 * 
+	 * @param node
+	 *            le noeud sélectionné
 	 */
 	public void afficherProprietes(DefaultMutableTreeNode node) {
 		Element elem = (Element) node.getUserObject();
 		String chemin = "/";
-		Folder Folder = elem.getDossierConteneur();
-		while (Folder != null) {
-			chemin = "/" + Folder.getNom() + chemin;
-			Folder = Folder.getDossierConteneur();
+		Folder folder = elem.getDossierConteneur();
+		while (folder != null) {
+			chemin = "/" + folder.getNom() + chemin;
+			folder = folder.getDossierConteneur();
 		}
 		String type = "";
 		if (elem.isFile()) {
 			type = "Fichier";
-		} else if (((Folder)elem).isModule()) {
+		} else if (((Folder) elem).isModule()) {
 			type = "Module";
 		} else {
 			type = "Dossier";
 		}
-		new DialogProprietes(null, elem.getNom(), type, chemin, elem.getDateCreation(), elem.getDateModification()).setVisible(true);
+		new DialogProprietes(null, elem.getNom(), type, chemin, elem
+				.getDateCreation(), elem.getDateModification())
+		.setVisible(true);
 	}
 
 	/**
 	 * Ouvre le fichier correspondant au noeud.
-	 * @param node le noeud sélectionné
+	 * 
+	 * @param node
+	 *            le noeud sélectionné
 	 */
 	public void ouvrir(DefaultMutableTreeNode node) {
-		//TODO ouvrir le fichier
-		System.out.println("ouverture du fichier "+node);
+		// TODO ouvrir le fichier
+		System.out.println("ouverture du fichier " + node);
 	}
 
-	/** La raine du JTree plan.*/
+	/** La raine du JTree plan. */
 	private DefaultMutableTreeNode racinePlan;
 
 	/**
 	 * Définit la racine du JTree plan.
-	 * @param racine la racine
+	 * 
+	 * @param racine
+	 *            la racine
 	 */
 	public void setRacineArbrePlan(DefaultMutableTreeNode racine) {
-	    this.racinePlan = racine;
+		this.racinePlan = racine;
 	}
 
 	/**
 	 * Mise à jour du JTree plan.
-	 * @param texte le texte à parser
+	 * 
+	 * @param texte
+	 *            le texte à parser
 	 */
 	public void updateTreePlan(String texte) {
-	    construireArbrePlan(this.racinePlan);
+		construireArbrePlan(this.racinePlan);
 	}
-	
+
 	/**
 	 * Construit l'arbre du plan de cours.
-	 * @param racine la racine de l'arbre
+	 * 
+	 * @param racine
+	 *            la racine de l'arbre
 	 */
 	public void construireArbrePlan(DefaultMutableTreeNode racine) {
-		//TODO parser le texte et ajouter les différents noeuds
+		// TODO parser le texte et ajouter les différents noeuds
 		System.out.println("Update du jtree!");
 	}
 
 	/**
-	 * 	
-	 * Method which provides a way create PDF  documents
-	 * @param outputFilePath The filePath of the output pdf
-	 * @param jTextPane The component to printout
+	 * 
+	 * Method which provides a way create PDF documents
+	 * 
+	 * @param outputFilePath
+	 *            The filePath of the output pdf
+	 * @param jTextPane
+	 *            The component to printout
 	 */
-	public static void printOutPdf(String outputFilePath,JTextPane jTextPane){
-		FileExporter.createPdf(false,jTextPane, outputFilePath);
+	public static void printOutPdf(String outputFilePath, JTextPane jTextPane) {
+		FileExporter.createPdf(false, jTextPane, outputFilePath);
 	}
-	
-	/* ---------------------------------------------------------*/
-	/* --------------Gestion du panel d'acceuil-----------------*/
-	/* ---------------------------------------------------------*/
+
+	/* --------------------------------------------------------- */
+	/* --------------Gestion du panel d'acceuil----------------- */
+	/* --------------------------------------------------------- */
 
 	/**
 	 * Accède à la liste des fichiers.
-	 * @param classerParCreation si la liste doit être classé par ordre de création
+	 * 
+	 * @param classerParCreation
+	 *            si la liste doit être classé par ordre de création
 	 * @return la liste des fichiers
 	 */
 	public List<Lesson> getListeFichiers(Boolean classerParCreation) {
 		LessonDao dao = new LessonDaoImpl();
 		return dao.findAll(classerParCreation);
 	}
-	
-	
-	
+
 	/*-----------------------------------------------------------*/
-	/*                     MainWindow function                   */
+	/* MainWindow function */
 	/*-----------------------------------------------------------*/
-	
+
 	/**
 	 * Add a work panel as a new tab.
-	 * @param name name of the module.
+	 * 
+	 * @param name
+	 *            name of the module.
 	 */
-	public static void addNewWorkPanel(String name){
+	public static void addNewWorkPanel(String name) {
 		MainWindow.getInstance().addNewWorkPanel(name);
 	}
-	
+
 	/**
 	 * Display text in status bar
+	 * 
 	 * @param text
 	 */
-	public void setLastAction(String text){
-		 MainWindow.getInstance().getStatusPanel().setStatus(text);
+	public void setLastAction(String text) {
+		MainWindow.getInstance().getStatusPanel().setStatus(text);
 	}
-		
+
 	/**
 	 * Swho the preferences dialog
 	 */
-	public void showParamDialog(){
+	public void showParamDialog() {
 		ParametersDialog.getInstance().setVisible(true);
 	}
 
 	/**
 	 * Print text on the view panel
-	 * @param msg : Text to show in the window
+	 * 
+	 * @param msg
+	 *            : Text to show in the window
 	 */
-	public void showText(String msg){
+	public void showText(String msg) {
 		JTextArea txtArea = ViewPanel.getViewPanel().getViewTextArea();
-		txtArea.setText(txtArea.getText()+ "\n" + msg);
+		txtArea.setText(txtArea.getText() + "\n" + msg);
 	}
-	
+
 	/**
 	 * Start Viavoic engine
 	 */
-	public void startEngine(){
-		//TODO appeler fonction moteur
+	public void startEngine() {
+		// TODO appeler fonction moteur
 	}
-	
+
 	/**
 	 * Stop viavoice engine
 	 */
-	public void stopEngine(){
-		//TODO appeler fonction moteur
+	public void stopEngine() {
+		// TODO appeler fonction moteur
 	}
-	
+
 	/**
 	 * Show the parameters dialog for dico
 	 */
-	public void showDicoParam(){
-		//TODO appeler fonction moteur
+	public void showDicoParam() {
+		// TODO appeler fonction moteur
 	}
-	
+
 	/**
 	 * Show the parameter dialog for egine options
 	 */
-	public void showOptParam(){
-		//TODO appeler fonction moteur
+	public void showOptParam() {
+		// TODO appeler fonction moteur
+	}
+	
+	
+	/**
+	 * Function that print error msg 
+	 * (Here to sysout)
+	 * @param msg
+	 */
+	public static void printLog(String msg){
+		System.out.println(msg);
 	}
 	
 	/**
-	 * Set the current panel displayed on screen modified/unmodified.
-	 * If true, a icon show that the panel has modification which haven't been saved.
-	 * @param modified true : display icon/ false hide icon
+	 * Function that print exception 
+	 * (Here to sysout)
+	 * @param msg
 	 */
-	public void setModified(boolean modified){
-		MainWindow.getInstance().getTabbedPaneHomeWork().setCurrentModified(modified);
+	public static void printLog(Exception e){
+		System.out.println(e.getMessage());
 	}
 	
+
+	/**
+	 * Set the current panel displayed on screen modified/unmodified. If true, a
+	 * icon show that the panel has modification which haven't been saved.
+	 * 
+	 * @param modified
+	 *            true : display icon/ false hide icon
+	 */
+	public void setModified(boolean modified) {
+		MainWindow.getInstance().getTabbedPaneHomeWork().setCurrentModified(
+				modified);
+	}
+
 	/**
 	 * After a "save all" action, all the tabbed pane are set to unmodified.
 	 */
-	public void setAllUnModified(){
+	public void setAllUnModified() {
 		MainWindow.getInstance().getTabbedPaneHomeWork().setAllUnModified();
 	}
-	
 
 	/*
 	 * GUI Parameters getters and setters
 	 */
-	
+
 	public String getWorkPlanPosition() {
 		return workPlanPosition;
 	}
@@ -432,5 +497,8 @@ public final class Controleur {
 	public boolean getWorkShowCourses() {
 		return workShowCourses;
 	}
+	
+	
+	
 
 }
--- Alma Speech Recognition/src/main/java/fr/alma/asr/gui/ParametersDialog.java
+++ Alma Speech Recognition/src/main/java/fr/alma/asr/gui/ParametersDialog.java
@@ -1,10 +1,10 @@
 package fr.alma.asr.gui;
+
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,32 +12,29 @@ import javax.swing.ComboBoxModel;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
-import javax.swing.JTabbedPane;
-
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+import javax.swing.JTabbedPane;
 
 /**
-* This code was edited or generated using CloudGarden's Jigloo
-* SWT/Swing GUI Builder, which is free for non-commercial
-* use. If Jigloo is being used commercially (ie, by a corporation,
-* company or business for any purpose whatever) then you
-* should purchase a license for each developer using Jigloo.
-* Please visit www.cloudgarden.com for details.
-* Use of Jigloo implies acceptance of these licensing terms.
-* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
-* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
-* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
-*/
-public class ParametersDialog extends javax.swing.JDialog {
+ * This code was edited or generated using CloudGarden's Jigloo SWT/Swing GUI
+ * Builder, which is free for non-commercial use. If Jigloo is being used
+ * commercially (ie, by a corporation, company or business for any purpose
+ * whatever) then you should purchase a license for each developer using Jigloo.
+ * Please visit www.cloudgarden.com for details. Use of Jigloo implies
+ * acceptance of these licensing terms. A COMMERCIAL LICENSE HAS NOT BEEN
+ * PURCHASED FOR THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED LEGALLY FOR
+ * ANY CORPORATE OR COMMERCIAL PURPOSE.
+ */
+public final class ParametersDialog extends javax.swing.JDialog {
 
 	{
-		//Set Look & Feel
+		// Set Look & Feel
 		try {
-			javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
-		} catch(Exception e) {
-			e.printStackTrace();
+			javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager
+					.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			Controleur.printLog(e);
 		}
 	}
 
@@ -47,31 +44,33 @@ public class ParametersDialog extends javax.swing.JDialog {
 	private JPanel spchRootPanel;
 	private JComboBox cbWokrPlanPosition;
 	private JLabel jLabel1;
-	
+
 	private static ParametersDialog instance;
 	private Controleur controleur = Controleur.getInstance();
-	
-	public static ParametersDialog getInstance(){
-		if(instance==null)
+
+	public static ParametersDialog getInstance() {
+		if (instance == null){
 			instance = new ParametersDialog();
+		}
 		return instance;
 	}
-	
+
 	private ParametersDialog() {
 		super(MainWindow.getInstance());
 		initGUI();
 	}
-	
+
 	private void initGUI() {
 		try {
 			{
 				this.setTitle("Paramètres");
 				this.setModal(true);
 				this.setLocationRelativeTo(MainWindow.getInstance());
-				
-				Dimension mainWindow =  MainWindow.getInstance().getSize();
-				setLocation((mainWindow.width - this.getSize().width)/2,(mainWindow.height - this.getSize().height)/2);
-								
+
+				Dimension mainWindow = MainWindow.getInstance().getSize();
+				setLocation((mainWindow.width - this.getSize().width) / 2,
+						(mainWindow.height - this.getSize().height) / 2);
+
 				JTabbedPane tabbedPane = new JTabbedPane();
 				getContentPane().setLayout(new GridLayout(1, 1));
 				getContentPane().add(tabbedPane);
@@ -81,72 +80,101 @@ public class ParametersDialog extends javax.swing.JDialog {
 					JPanel guiParamPane = new JPanel();
 					GridBagLayout thisLayout = new GridBagLayout();
 					guiParamPane.setLayout(thisLayout);
-					tabbedPane.addTab("Interface Graphique", null, guiParamPane, null);
+					tabbedPane.addTab("Interface Graphique", null,
+							guiParamPane, null);
 					{
 						labelWorkPlanPanel = new JLabel();
-						guiParamPane.add(labelWorkPlanPanel, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-						labelWorkPlanPanel.setText("Position du plan par défaut");
+						guiParamPane.add(labelWorkPlanPanel,
+								new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0,
+										GridBagConstraints.EAST,
+										GridBagConstraints.NONE, new Insets(0,
+												0, 0, 0), 0, 0));
+						labelWorkPlanPanel
+								.setText("Position du plan par défaut");
 					}
 					{
 						jLabel1 = new JLabel();
-						guiParamPane.add(jLabel1, new GridBagConstraints(2, 3, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+						guiParamPane.add(jLabel1, new GridBagConstraints(2, 3,
+								1, 1, 0.0, 0.0, GridBagConstraints.EAST,
+								GridBagConstraints.NONE,
+								new Insets(0, 0, 0, 0), 0, 0));
 					}
 					{
 						cbShowPlan = new JCheckBox();
-						guiParamPane.add(cbShowPlan, new GridBagConstraints(2, 3, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 20, 0, 0), 0, 0));
+						guiParamPane.add(cbShowPlan, new GridBagConstraints(2,
+								3, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
+								GridBagConstraints.NONE,
+								new Insets(0, 20, 0, 0), 0, 0));
 						cbShowPlan.setText("Afficher Plan");
 						cbShowPlan.addActionListener(new ActionListener() {
 							public void actionPerformed(ActionEvent evt) {
-								controleur.setWorkShowPlan(((JCheckBox)evt.getSource()).isSelected());
+								controleur.setWorkShowPlan(((JCheckBox) evt
+										.getSource()).isSelected());
 							}
 						});
 					}
 					{
-						ComboBoxModel cbWokrPlanPositionModel = 
-							new DefaultComboBoxModel(
-									new String[] { "Droite", "Gauche" });
+						ComboBoxModel cbWokrPlanPositionModel = new DefaultComboBoxModel(
+								new String[] { "Droite", "Gauche" });
 						cbWokrPlanPosition = new JComboBox();
-						guiParamPane.add(cbWokrPlanPosition, new GridBagConstraints(2, 2, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 20, 0, 0), 0, 0));
+						guiParamPane.add(cbWokrPlanPosition,
+								new GridBagConstraints(2, 2, 1, 1, 0.0, 0.0,
+										GridBagConstraints.WEST,
+										GridBagConstraints.NONE, new Insets(0,
+												20, 0, 0), 0, 0));
 						cbWokrPlanPosition.setModel(cbWokrPlanPositionModel);
-						cbWokrPlanPosition.addActionListener(new ActionListener() {
-							public void actionPerformed(ActionEvent evt) {
-								controleur.setWorkPlanPosition(((JComboBox)evt.getSource()).getSelectedItem().toString());
-							}
-						});
+						cbWokrPlanPosition
+								.addActionListener(new ActionListener() {
+									public void actionPerformed(ActionEvent evt) {
+										controleur
+												.setWorkPlanPosition(((JComboBox) evt
+														.getSource())
+														.getSelectedItem()
+														.toString());
+									}
+								});
 					}
 					{
 						cbShowCourses = new JCheckBox();
-						guiParamPane.add(cbShowCourses, new GridBagConstraints(2, 4, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 20, 0, 0), 0, 0));
+						guiParamPane.add(cbShowCourses, new GridBagConstraints(
+								2, 4, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
+								GridBagConstraints.NONE,
+								new Insets(0, 20, 0, 0), 0, 0));
 						cbShowCourses.setText("Afficher les modules");
 						cbShowCourses.addActionListener(new ActionListener() {
 							public void actionPerformed(ActionEvent evt) {
-								controleur.setWorkShowCourses(((JCheckBox)evt.getSource()).isSelected());
+								controleur.setWorkShowCourses(((JCheckBox) evt
+										.getSource()).isSelected());
 							}
 						});
 					}
-					thisLayout.rowWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1};
-					thisLayout.rowHeights = new int[] {18, 30, 21, 22, 22, 28, 24, 20};
-					thisLayout.columnWeights = new double[] {0.0, 0.0, 0.0, 0.1};
-					thisLayout.columnWidths = new int[] {32, 176, 201, 7};
+					thisLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0,
+							0.0, 0.0, 0.0, 0.1 };
+					thisLayout.rowHeights = new int[] { 18, 30, 21, 22, 22, 28,
+							24, 20 };
+					thisLayout.columnWeights = new double[] { 0.0, 0.0, 0.0,
+							0.1 };
+					thisLayout.columnWidths = new int[] { 32, 176, 201, 7 };
 				}
 				{
 					spchRootPanel = new JPanel();
-					tabbedPane.addTab("Reconnaissance vocale", null, spchRootPanel, null);
+					tabbedPane.addTab("Reconnaissance vocale", null,
+							spchRootPanel, null);
 				}
 			}
 			pack();
 		} catch (Exception e) {
-			e.printStackTrace();
+			Controleur.printLog(e);
 		}
 	}
 
 	@Override
-	public void setVisible(boolean b){
+	public void setVisible(boolean b) {
 		setValues();
 		super.setVisible(b);
 	}
-	
-	public void setValues(){
+
+	public void setValues() {
 		cbShowCourses.setSelected(controleur.getWorkShowCourses());
 		cbWokrPlanPosition.setSelectedItem(controleur.getWorkPlanPosition());
 		cbShowPlan.setSelected(controleur.getWorkShowPlan());
